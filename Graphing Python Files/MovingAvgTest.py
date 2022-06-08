fae0f1b295291c5bf737babd77b9200e0d7d998e
crab-milk <72271395+crab-milk@users.noreply.github.com>
2022-06-08 12:24:14 -0500

    Added time unit conversions
    The code automatically converts the time from ms to minutes, seconds, and to hours, depending on how long the CSV file records data.

--- Graphing Python Files/MovingAvgTest.py
+++ Graphing Python Files/MovingAvgTest.py
@@ -23,7 +23,6 @@ def read_file():
         # Add first row
         header.append(row)
         break
-    print(header)
 
     # Gather data from CSV file
     lines = csvreader
@@ -31,8 +30,6 @@ def read_file():
         if not(row[1] == " START " or row[1] == " RECALIBRATE " or  row[1] == " NEW SENSOR DELAY " or row[1] == " PAUSE "):
             raw_time.append(row[0])
             press_diff.append(float(row[1]))
-    print(raw_time)
-    print(press_diff)
 
     # Finished reading a file
     file.close()
@@ -50,7 +47,7 @@ def processData(raw_data):
 
     # Scale data by this amount of time
     try:
-        avg_duration = eval(input("How Much time Between Intervals (in ms): "))
+        avg_duration = eval(input("How Much Time Between Intervals (in ms): "))
     except NameError:
         print("Invalid input")
 
@@ -79,9 +76,9 @@ def getAvgData(unscaled_data, avg_duration):
             press_sum = press_sum + unscaled_data[1][i]
             count = count + 1
         else:
-            # Terminate program if the time passed as parameter is too small
+            # Show raw data if the time passed as parameter is too small
             if count == 0:
-                sys.exit("The time you wanted to use is too small for this data set.")
+                return unscaled_data
             else:
                 # Append avg values
                 avg_time.append(time_sum / count)
@@ -95,20 +92,42 @@ def getAvgData(unscaled_data, avg_duration):
     return [avg_time, avg_press_diff]
 
 # ***** GRAPHING *****
-def graphData(proc_data):
+def graph_data(proc_data):
+    # Change the units of the time automatically
+    unit_in_ms = 0
+    unit_name = "ms"
+    if proc_data[0][len(proc_data[0]) - 1] >= 86400000:
+        # Unit changed to hours
+        unit_name = "hr"
+        unit_in_ms = 3600000
+    elif proc_data[0][len(proc_data[0]) - 1] >= 1200000:
+        # Unit changed to minutes
+        unit_name = "min"
+        unit_in_ms = 60000
+    elif proc_data[0][len(proc_data[0]) - 1] >= 60000:
+        # Unit changed to seconds
+        unit_name = "s"
+        unit_in_ms = 1000
+    proc_data = change_graph_units(proc_data, unit_in_ms)
+    
     # Graph values onto a plot
     plt.plot(proc_data[0], proc_data[1], color = 'b', linestyle = 'dashed', marker = 'o', label = "Control")
     plt.xticks(rotation = 25)
-
+    
     # Display graph
     plt.title('Pressure Over Time', fontsize = 20)
-    plt.xlabel('Time (ms)')
+    plt.xlabel('Time (' + unit_name + ')')
     plt.ylabel('Pressure')
-
     plt.grid()
     plt.legend()
     plt.show()
 
+def change_graph_units(proc_data, unit_in_ms):
+    if unit_in_ms != 0:
+        for i in range(len(proc_data[0])):
+            proc_data[0][i] = proc_data[0][i] / unit_in_ms
+    return proc_data
+
 raw_data = read_file()
 proc_data = processData(raw_data)
-graphData(proc_data)
+graph_data(proc_data)
