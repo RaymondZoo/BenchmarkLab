8d000dd2d549350dc8d0cbfdec7852f907d20ca7
crab-milk <72271395+crab-milk@users.noreply.github.com>
2022-06-10 11:32:07 -0500

    Added read line input functions
    The code should now be able to read the CSV line inputs without actually opening and closing the files.

---
+++ Graphing Python Files/AutomaticGraphingProgram.py
@@ -0,0 +1,167 @@
+from ast import Constant
+from datetime import datetime
+from tokenize import String
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+# Variables, must only be altered by reading line inputs
+raw_time = []
+press_diff = []
+
+# ***** READING LINE INPUTS *****
+def read_line_inputs(str_read):
+    # Prerequisite: ONLY PUT IN DATA THAT HAS NUMBERS
+    col = 0
+    new_data = True
+    
+    # Takes a line from a CSV input, reads it
+    # Appends the data to the proper list
+    for i in range(0, len(str_read)):
+        character = str_read[i]
+        if character == ",":
+            col = col + 1
+            new_data = True
+        else:
+            # Update time if we are reading time
+            if col == 0:
+                if new_data:
+                    # New time, append to raw_time
+                    raw_time.append(character)
+                    new_data = False
+                else:
+                    # Adding to current time string
+                    raw_time[len(raw_time) - 1] += character
+            elif col == 1:
+                if new_data:
+                    # New pressure data
+                    press_diff.append(character)
+                    new_data = False
+                else:
+                    # Adding to current data string
+                    press_diff[len(raw_time) - 1] += character
+    press_diff[len(raw_time) - 1] = float(press_diff[len(raw_time) - 1])
+    return [raw_time, press_diff]
+
+# ***** READING CSV FILE *****
+def read_file():
+    # CSV values
+    raw_time_f = []
+    press_diff_f = []
+
+    # Open file
+    file = open('testdata.csv', encoding = 'utf-8-sig')
+    type(file)
+    csvreader = csv.reader(file)
+
+    # Read all of the headers
+    header = []
+    for row in csvreader:     
+        # Add first row
+        header.append(row)
+        break
+
+    # Gather data from CSV file
+    lines = csvreader
+    for row in lines:
+        if not(row[1] == " START " or row[1] == " RECALIBRATE " or  row[1] == " NEW SENSOR DELAY " or row[1] == " PAUSE "):
+            raw_time_f.append(row[0])
+            press_diff_f.append(float(row[1]))
+
+    # Finished reading a file
+    file.close()
+    return [raw_time_f, press_diff_f]
+
+# ***** PROCESSING DATA *****
+def process_data(raw_data):
+    # Variables for graphing
+    proc_time = []
+
+    # Process the raw time into intervals
+    for i in range(len(raw_data[0])):
+        time = (datetime.strptime(raw_data[0][i], '%Y-%m-%d %H:%M:%S.%f') - datetime.strptime(raw_data[0][0], '%Y-%m-%d %H:%M:%S.%f')).total_seconds() * 1000
+        proc_time.append(float(time))
+
+    # Scale data by this amount of time
+    avg_duration = 0
+
+    # Graph raw data if avg_duration is not specified;
+    # Otherwise, get average of time interval
+    if avg_duration == 0:
+        return [proc_time, raw_data[1]]
+    else:
+        return get_avg_data([proc_time, raw_data[1]], avg_duration)
+
+def get_avg_data(unscaled_data, avg_duration):
+    # Variables
+    avg_time = []
+    avg_press_diff = []
+    
+    start_time = 0
+    nodes = 0
+    time_sum = 0
+    press_sum = 0
+    
+    # Do a loop start beginning of the time  to increase avg_duration
+    for i in range(len(unscaled_data[0])):
+        if (unscaled_data[0][i] >= start_time and unscaled_data[0][i] < start_time + avg_duration):
+            # Update sums
+            time_sum = time_sum + unscaled_data[0][i]
+            press_sum = press_sum + unscaled_data[1][i]
+            nodes = nodes + 1
+        else:
+            # Show raw data if the time passed as parameter is too small
+            if nodes == 0:
+                return unscaled_data
+            else:
+                # Append avg values
+                avg_time.append(time_sum / nodes)
+                avg_press_diff.append(press_sum / nodes)
+                
+                # Reset values
+                start_time = unscaled_data[0][i]
+                nodes = 0
+                time_sum = 0
+                press_sum = 0
+    return [avg_time, avg_press_diff]
+
+# ***** GRAPHING *****
+def graph_data(proc_data):
+    # Change the units of the time automatically
+    unit_in_ms = 0
+    unit_name = "ms"
+    if proc_data[0][len(proc_data[0]) - 1] >= 86400000:
+        # Unit changed to hours
+        unit_name = "hr"
+        unit_in_ms = 3600000
+    elif proc_data[0][len(proc_data[0]) - 1] >= 1200000:
+        # Unit changed to minutes
+        unit_name = "min"
+        unit_in_ms = 60000
+    elif proc_data[0][len(proc_data[0]) - 1] >= 60000:
+        # Unit changed to seconds
+        unit_name = "s"
+        unit_in_ms = 1000
+    proc_data = change_graph_units(proc_data, unit_in_ms)
+    
+    # Graph values onto a plot
+    plt.plot(proc_data[0], proc_data[1], color = 'b', linestyle = 'dashed', marker = 'o', label = "Control")
+    plt.xticks(rotation = 25)
+    
+    # Display graph
+    plt.title('Pressure Over Time', fontsize = 20)
+    plt.xlabel('Time (' + unit_name + ')')
+    plt.ylabel('Pressure')
+    plt.grid()
+    plt.legend()
+    plt.show() # Shows the graph
+
+def change_graph_units(proc_data, unit_in_ms):
+    if unit_in_ms != 0:
+        for i in range(len(proc_data[0])):
+            proc_data[0][i] = proc_data[0][i] / unit_in_ms
+    return proc_data
+
+# raw_data = read_file()
+# proc_data = process_data(raw_data)
+# graph_data(proc_data)
