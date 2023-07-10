c0a4c82ece53e75c7a9ac792b32ee3575a7dad71
RaymondZoo <ray.yijun.zou@gmail.com>
2023-07-10 14:36:53 -0500

    moretempsensor

--- Arduino Files/InterfaceProjectv2/InterfaceProjectv2.ino
+++ Arduino Files/InterfaceProjectv2/InterfaceProjectv2.ino
@@ -20,9 +20,9 @@ float pressureZeroI;
 float pressureZeroO;
 
 //LCD setup
-//const LiquidCrystal_I2C lcd(0x27, 16, 2); //sets the LCD I2C communication address; format(address, columns, rows)
-//const String dataLabel1 = "PressureIn";
-//const String dataLabel2 = "PressureOut";
+const LiquidCrystal_I2C lcd(0x27, 16, 2); //sets the LCD I2C communication address; format(address, columns, rows)
+const String dataLabel1 = "PressureIn";
+const String dataLabel2 = "PressureOut";
 
 //temperature sensor setup
 int ktcSO = 8;
@@ -31,40 +31,20 @@ int ktcCLK = 10;
 
 MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
 
+//temperature sensor 2 setup
+int ktcSO2 = 5;
+int ktcCS2 = 6;
+int ktcCLK2 = 7;
+MAX6675 ktc2(ktcCLK2, ktcCS2, ktcSO2);
+
 void setup() //setup routine, runs once when system turned on or reset
 {
   Serial.begin(baudRate); //initializes serial communication at set baud rate bits per second
-  //lcd.init(); //initializes the LCD screen
-  //lcd.init();
-  //lcd.backlight();
+  lcd.init(); //initializes the LCD screen
+  lcd.backlight();
 
   delay(500);
 
-  //  if (Serial.available()) // if there is data coming
-  // {
-  //   String command = Serial.readStringUntil('\n'); 
-  //   command.trim();
-  //   if (command.substring(0, 2) == "sd")
-  //   {
-  //     sensorreadDelay = command.substring(3).toInt();
-  //   }
-
-  //   if (command.substring(0, 3) == "psi")
-  //   {
-  //     pressuretransducermaxPSI = command.substring(4).toInt();
-  //   }
-
-  // }
-  //pinMode(13, OUTPUT); // set the digital pin as output:
-  //Serial.print(dataLabel1);
-  //Serial.print(",");
-  //Serial.print(dataLabel2);
-  //Serial.print(",");
-  //Serial.println("PressureDifference");
-
-  //calibration
-  //pressureZeroI = analogRead(pressureIn);
-  //pressureZeroO = analogRead(pressureOut);
 }
 
 void loop() //loop routine runs over and over again forever
@@ -97,7 +77,7 @@ void loop() //loop routine runs over and over again forever
   pressureOutValue = ((singleOUT - pressureZeroO) * pressuretransducermaxPSI) / (pressureMax - pressureZeroO); //conversion equation to convert analog reading to psi
   pressureDifference = pressureOutValue - pressureInValue;
 
-  /*
+  
   lcd.setCursor(0, 0); //sets cursor to column 0, row 0
   lcd.print("I:"); //prints label
   lcd.print(pressureInValue, 2); //prints pressure value to lcd screen, 1 digit on float
@@ -108,7 +88,7 @@ void loop() //loop routine runs over and over again forever
   lcd.setCursor(0, 1);
   lcd.print("D:");
   lcd.print(pressureDifference, 2); //prints pressure value to lcd screen, 1 digit on float
-  */
+  
 
   Serial.print(pressureInValue); //prints value from previous line to serial
   Serial.print(","); //prints value from previous line to serial
@@ -116,6 +96,8 @@ void loop() //loop routine runs over and over again forever
   Serial.print(","); //prints value from previous line to serial
   Serial.print(pressureDifference); //prints value from previous line to serial
   Serial.print(",");
-  Serial.println(ktc.readCelsius());
+  Serial.print(ktc.readCelsius());
+  Serial.print(",");
+  Serial.println(ktc2.readCelsius());
   delay(sensorreadDelay); //delay in milliseconds between read values
 }
